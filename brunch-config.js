8d5323897d65e75b775108fd7887aadff7b080dd
jacquelinclem <hello@jacquelinclement.com>
2018-06-04 15:16:20 +0200

    Wip

--- brunch-config.js
+++ brunch-config.js
@@ -1,6 +1,6 @@
 exports.config = {
   paths: {
-    watched: ['main.scss'],
+    watched: ['main.scss','assets'],
     public: "dist"
   },
   files: {
@@ -11,17 +11,31 @@ exports.config = {
   plugins: {
     postcss: {
       processors: [
-        require('autoprefixer')(['last 8 versions']),
-        require('csswring')()
+        require('autoprefixer')(['last 8 versions'])
+
       ]
     },
     sass: {
       mode: 'ruby'
     },
     copycat:{
-      "assets": ["dist"],
+      "/assets": ["assets"],
       verbose : false, //shows each file that is copied to the destination directory
       onlyChanged: true //only copy a file if it's modified time has changed (only effective when using brunch watch)
+    }
+  },
+  overrides: {
+    production: {
+      optimize: true,
+      sourceMaps: false,
+      plugins: {
+        postcss: {
+          processors: [
+            require('autoprefixer')(['last 8 versions']),
+            require('csswring')()
+          ]
+        },
+      }
     }
   }
 };
